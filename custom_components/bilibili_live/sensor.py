968740ddbfa5d715794221f22a6f4bfe2794206f
Posartor <bing5796@qq.com>
2023-05-31 22:06:52 +0800

    Updated to new API with WBI Authentication

--- custom_components/bilibili_live/sensor.py
+++ custom_components/bilibili_live/sensor.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import time
+import urllib.parse
+from hashlib import md5
+from functools import reduce
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME
@@ -8,12 +12,47 @@ import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, CONF_UID
 
+mixinKeyEncTab = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
+    33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
+    61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+    36, 20, 34, 44, 52
+]
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_UID): cv.string,
-        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
     }
 )
+
+def getMixinKey(orig: str):
+    return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, '')[:32]
+
+def encWbi(params: dict, img_key: str, sub_key: str):
+    mixin_key = getMixinKey(img_key + sub_key)
+    curr_time = round(time.time())
+    params['wts'] = curr_time
+    params = dict(sorted(params.items()))
+    params = {
+        k : ''.join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v 
+        in params.items()
+    }
+    query = urllib.parse.urlencode(params)
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()
+    params['w_rid'] = wbi_sign
+    return params
+
+def getWbiKeys():
+    resp = requests.get('https://api.bilibili.com/x/web-interface/nav')
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content['data']['wbi_img']['img_url']
+    sub_url: str = json_content['data']['wbi_img']['sub_url']
+    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    return img_key, sub_key
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     uid = config_entry.data["uid"]
@@ -53,11 +92,12 @@ class BilibiliLiveSensor(Entity):
         return "mdi:television-classic"
 
     def update(self):
-        url = f"https://api.bilibili.com/x/space/acc/info?mid={self._uid}"
+        img_key, sub_key = getWbiKeys()
+        params = {"mid": self._uid, "token": "", "platform": "web", "web_location": "1550101"}
+        signed_params = encWbi(params, img_key, sub_key)
+        url = f"https://api.bilibili.com/x/space/wbi/acc/info?" + urllib.parse.urlencode(signed_params)
         headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15",
-            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15"
         }
         response = requests.get(url, headers=headers)
         data = json.loads(response.text)
